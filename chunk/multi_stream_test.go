3a9ad8b4938726d55886a6b56d3be5463f2a699a
Taylor Blau <me@ttaylorr.com>
2016-04-24 14:32:50 -0400

    chunk/multi_stream: add newMockStream func to tests

--- chunk/multi_stream_test.go
+++ chunk/multi_stream_test.go
@@ -8,14 +8,8 @@ import (
 )
 
 func TestMultiStreamAppendsMultipleChunkStreams(t *testing.T) {
-	c1 := make(chan *chunk.Chunk)
-	c2 := make(chan *chunk.Chunk)
-
-	s1 := new(MockStream)
-	s1.On("In").Return(c1).Once()
-
-	s2 := new(MockStream)
-	s2.On("In").Return(c2).Once()
+	s1, c1 := newMockStream()
+	s2, c2 := newMockStream()
 
 	ms := chunk.NewMultiStream()
 	ms.Append(s1, s2)
@@ -30,4 +24,13 @@ func TestMultiStreamAppendsMultipleChunkStreams(t *testing.T) {
 
 	assert.Equal(t, o2, <-ms.In())
 	assert.Equal(t, o1, <-ms.In())
+}
+
+func newMockStream() (*MockStream, chan *chunk.Chunk) {
+	s := new(MockStream)
+	c := make(chan *chunk.Chunk)
+
+	s.On("In").Return(c).Once()
+
+	return s, c
 }
